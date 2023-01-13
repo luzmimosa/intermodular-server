39e1ab4db25ad82a3e30e20b36c033608a68a633
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-01-13 19:04:33 +0100

    added serverOptions

--- src/ConfigurationProvider.ts
+++ src/ConfigurationProvider.ts
@@ -1,7 +1,11 @@
-export const databaseInfo = {
+export const databaseOptions = {
     host: '127.0.0.1',
     port: 27017,
     database: 'intermodular',
     user: 'server',
     password: 'server'
 };
+
+export const serverOptions = {
+    listeningPort: 3000
+}
