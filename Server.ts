51f2dd94049e32458f03bcbc3be88d33c6fe4bb7
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-01-13 19:05:32 +0100

    improved and clarified the startup sequence

--- Server.ts
+++ Server.ts
@@ -4,55 +4,63 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import {connectToDatabase} from "./src/database/GlobalDatabase";
-import {createUser} from "./src/database/model/user/UserManager";
+import {serverOptions} from "./src/ConfigurationProvider";
 
 
 // routes constants
 
 export const SERVER = express();
 
-// view engine setup
-SERVER.set('views', path.join(__dirname, 'views'));
-SERVER.set('view engine', 'pug');
-
-SERVER.use(logger('dev'));
-SERVER.use(express.json());
-SERVER.use(express.urlencoded({ extended: false }));
-SERVER.use(cookieParser());
-SERVER.use(express.static(path.join(__dirname, 'public')));
+startupSequence();
 
 
-//forward to error handler
-SERVER.use((req: any, res: any, next: any) => {
-    next(createError(404));
-});
+// Functions
 
-// error handler
-SERVER.use((err: any, req: any, res: any) => {
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+async function startupSequence() {
+    // database
+    if (!(await setupDatabase())) return;
 
-SERVER.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+    //server
+    setupServer();
+}
 
-connectToDatabase(() => {
-    createUser({
-        username: "pauetekkuet",
-        displayName: "Pau ~ 💛",
-        profilePicture: "default_profile_picture.png",
-        biography: "Hey there, I'm using aplicación intermodular sin nombre",
-        creationDatetime: Date.now(),
-        featuredRoutes: [],
-        toDoRoutes: [],
-        email: "uwu@gmail.com",
-        passwordHash: "edf9cf90718610ee7de53c0dcc250739239044de9ba115bb0ca6026c3e4958a5",
-        devices: []
-    }, () => {
-        console.log('User created');
-    }, (error) => {
+async function setupDatabase() {
+    let databaseConnected = true;
+    await connectToDatabase((error) => {
         console.error(error);
+        databaseConnected = false;
     })
-});
+
+    return databaseConnected;
+}
+
+function setupServer() {
+    // view engine setup
+    SERVER.set('views', path.join(__dirname, 'views'));
+    SERVER.set('view engine', 'pug');
+
+    SERVER.use(logger('dev'));
+    SERVER.use(express.json());
+    SERVER.use(express.urlencoded({ extended: false }));
+    SERVER.use(cookieParser());
+    SERVER.use(express.static(path.join(__dirname, 'public')));
+
+
+    //forward to error handler
+    SERVER.use((req: any, res: any, next: any) => {
+        next(createError(404));
+    });
+
+    // error handler
+    SERVER.use((err: any, req: any, res: any) => {
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
+
+    const port = serverOptions.listeningPort;
+
+    SERVER.listen(port, () => {
+        console.log('Server is running on port', port);
+    });
+}
