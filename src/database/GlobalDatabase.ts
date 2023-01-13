b3ca92b740e79b62fec40ddba4020073afaec5c1
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-01-13 19:04:08 +0100

    strictQuery mongoose option setted to true

--- src/database/GlobalDatabase.ts
+++ src/database/GlobalDatabase.ts
@@ -1,18 +1,17 @@
 import {connect} from 'mongoose';
-import {databaseInfo} from "../ConfigurationProvider";
+import {databaseOptions} from "../ConfigurationProvider";
+import * as mongoose from "mongoose";
 
 export async function connectToDatabase(
-    onConnect: () => void = () => {},
     onError: (error: string) => void = () => {}
 ) {
     console.log("Connecting to database");
+    mongoose.set('strictQuery', true);
 
-    await connect(`mongodb://${databaseInfo.host}:${databaseInfo.port}/${databaseInfo.database}`)
-        .then(() => {
-            console.log("Connected to database");
-            onConnect();
-        }, (err: any) => {
-            console.log("Error connecting to database");
-            onError(err);
-        });
+    await connect(
+        `mongodb://${databaseOptions.host}:${databaseOptions.port}/${databaseOptions.database}`
+    ).catch((error: any) => {
+        onError(error);
+    })
+
 }
