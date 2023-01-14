1f9ca5d456227697f908f9acf3f536beb56c877e
BuildTools <unconfigured@null.spigotmc.org>
2023-01-14 02:41:53 +0100

    user query logic added

--- src/api/endpoints/GetUserEndpoint.ts
+++ src/api/endpoints/GetUserEndpoint.ts
@@ -1,14 +1,24 @@
 import {Endpoint} from "../EndpointRegister";
+import {userByUsername} from "../../database/model/user/UserManager";
 
 
 export const getUserEndpoint = {
     method: "GET",
     path: "v1/user",
-    onCall: (args, req, res) => {
-        let response = "HI!";
-        for (let argument of args) {
-            response += "(" + argument + ")";
+    onCall: async (args, req, res) => {
+        if (args.length < 1) {
+            res.status(400).send("Missing arguments");
+            return;
         }
-        res.send(response);
+
+        const username = args[0];
+
+        const databaseUser = await userByUsername(username);
+        if (!databaseUser) {
+            res.status(404).json({error: "User not found"});
+            return;
+        }
+
+        res.status(200).json(databaseUser);
     }
 } as Endpoint;
