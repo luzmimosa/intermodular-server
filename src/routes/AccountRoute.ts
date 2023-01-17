48c7d7b6799accabd029fd9acc28ae34c5821a9c
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-01-17 13:06:27 +0100

    /login route implementes. Now gives bearer tokens. Improved /register route by forcing parameters to exist.

--- src/routes/AccountRoute.ts
+++ src/routes/AccountRoute.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import {registerUser} from "../auth/UserRegistrator";
+import {credentialsMatch, userByUsername} from "../database/model/user/UserManager";
+import {User} from "../database/model/user/UserModel";
+import {sign} from "jsonwebtoken";
 
 export const accountRouter = express.Router();
 
@@ -7,13 +10,13 @@ accountRouter.post("/account", (req, res) => {
     res.status(200).json({ message: "Hello from account" });
 })
 
-accountRouter.post("/account/register", (req, res) => {
+accountRouter.post("/account/register", async (req, res) => {
     try {
-        const username = req.body.username;
-        const displayName = req.body.displayName;
-        const biography = req.body.biography;
-        const email = req.body.email;
-        const password = req.body.password;
+        const username: string = req.body.username !!;
+        const displayName: string = req.body.displayName ?? username;
+        const biography: string = req.body.biography !!;
+        const email: string = req.body.email !!;
+        const password: string = req.body.password !!;
 
         registerUser(username, displayName, biography, email, password, (result, isError) => {
             console.log("Response: " + result);
@@ -29,6 +32,55 @@ accountRouter.post("/account/register", (req, res) => {
     }
 })
 
-accountRouter.post("/account/login", (req, res) => {
-    res.status(200).json({ message: "Hello from account login" });
+accountRouter.post("/account/login", async (req, res) => {
+
+    try {
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password !!;
+
+        let match: boolean;
+        let user: User | null;
+
+        if (username) {
+            match = await credentialsMatch.byUsername(username, password);
+            user = await userByUsername(username);
+        } else if (email) {
+            match = await credentialsMatch.byEmail(email, password);
+            user = await userByUsername(email);
+        } else {
+            res.status(400).json({ message: "MISSING_PARAMS" });
+            return;
+        }
+
+        if (!match || !user) {
+            res.status(400).json({ message: "INVALID_CREDENTIALS" });
+            return;
+        }
+
+        const token = generateToken(user);
+
+        res.status(200).setHeader("Authorization", token).json({ message: "OK" });
+
+    } catch (error: any) {
+        console.log(error);
+        res.status(400).json({ message: "MISSING_PARAMS" });
+    }
 })
+
+function generateToken(
+    user: User,
+    expiresIn: number = 60 * 60 * 24 * 7 // 7 days
+): string {
+    return sign(
+        {
+            username: user.username,
+            creationTime: Date.now(),
+        },
+        process.env.JWT_SECRET !!,
+        {
+            expiresIn: expiresIn,
+            algorithm: "HS256"
+        }
+    );
+}
