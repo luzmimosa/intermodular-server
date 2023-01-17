3ee973500380c28b868e469eb1e1e1c2996c96fc
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-01-17 20:40:34 +0100

    post, put and delete verbs are now supported

--- src/routes/ApiRoute.ts
+++ src/routes/ApiRoute.ts
@@ -7,4 +7,22 @@ apiRouter.get('/api/*', async (req, res) => {
     const apiEndpoint = req.url.replace("/api/", "");
 
     await handleRequest(apiEndpoint, "GET", req, res);
-})
+});
+
+apiRouter.post('/api/*', async (req, res) => {
+    const apiEndpoint = req.url.replace("/api/", "");
+
+    await handleRequest(apiEndpoint, "POST", req, res);
+});
+
+apiRouter.put('/api/*', async (req, res) => {
+    const apiEndpoint = req.url.replace("/api/", "");
+
+    await handleRequest(apiEndpoint, "PUT", req, res);
+});
+
+apiRouter.delete('/api/*', async (req, res) => {
+    const apiEndpoint = req.url.replace("/api/", "");
+
+    await handleRequest(apiEndpoint, "DELETE", req, res);
+});
