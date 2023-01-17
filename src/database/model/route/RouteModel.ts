69430173dca0b0c24ef41019143103e27523c806
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-01-17 20:42:19 +0100

    changed route enums; added route lenght calculation

--- src/database/model/route/RouteModel.ts
+++ src/database/model/route/RouteModel.ts
@@ -30,20 +30,20 @@ export interface Waypoint {
 }
 
 export enum RouteType {
-    WALK = "walk",
-    TREEKING = "trekking",
-    RUNNING = "running",
-    BYCYCLE = "bycicle",
-    PHOTOGRAPHY = "photography",
-    OTHER = "other"
+    WALKING = "WALKING",
+    TREEKING = "TREEKING",
+    RUNNING = "RUNNING",
+    BYCYCLE = "BYCYCLE",
+    PHOTOGRAPHY = "PHOTOGRAPHY",
+    OTHER = "OTHER"
 }
 
 export enum RouteDifficulty {
-    TRIVIAL = "trivial",
-    EASY = "easy",
-    MEDIUM = "medium",
-    HARD = "hard",
-    EXPERT = "expert"
+    TRIVIAL = "TRIVIAL",
+    EASY = "EASY",
+    MEDIUM = "MEDIUM",
+    HARD = "HARD",
+    EXPERT = "EXPERT"
 }
 
 const waypointSchema = new Schema({
@@ -71,3 +71,26 @@ const routeSchema = new Schema({
 });
 
 export const RouteModel = mongoose.model('route', routeSchema);
+
+export function calculateRouteLenght(measures: GpsMeasure[]) {
+    let total = 0;
+    for (let i = 0; i < measures.length - 1; i++) {
+        total += calculateDistance(measures[i], measures[i + 1]);
+    }
+    return total;
+}
+
+function calculateDistance(a: GpsMeasure, b: GpsMeasure) {
+    const R = 6371e3; // metres
+    const angle1 = a.latitude * Math.PI / 180; // φ, λ in radians
+    const angle2 = b.latitude * Math.PI / 180;
+    const angle3 = (b.latitude - a.latitude) * Math.PI / 180;
+    const angle4 = (b.longitude - a.longitude) * Math.PI / 180;
+
+    const angle = Math.sin(angle3 / 2) * Math.sin(angle3 / 2) +
+        Math.cos(angle1) * Math.cos(angle2) *
+        Math.sin(angle4 / 2) * Math.sin(angle4 / 2);
+    const c = 2 * Math.atan2(Math.sqrt(angle), Math.sqrt(1 - angle));
+
+    return R * c; // in metres
+}
