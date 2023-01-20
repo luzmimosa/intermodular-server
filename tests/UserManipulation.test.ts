eee800d37d5c2b904e9a749b6a85ab08d9e8bef6
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-01-20 02:03:27 +0100

    test for user registration. It should also perform more user-related tests in the future

---
+++ tests/UserManipulation.test.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+
+describe('user creation', () => {
+    it ('should register an user by using the "/account/register" endpoint ', async () => {
+
+        const userNumber = Math.round(Math.random() * 100000)
+        const user = {
+            username: "alpacaDePruebas" + userNumber,
+            displayName: "Alpaca",
+            biography: "Soy una alpaca preciosa UwU",
+            email: "alpaca" + userNumber + "@gmail.com",
+            password: "alpacontraseña"
+        }
+
+        const response = await request('https://localhost')
+            .post('/account/register')
+            .send(user)
+            .set('Accept', 'application/json')
+            .trustLocalhost(true)
+            .expect(200)
+            .expect('Content-Type', /json/);
+
+        expect(response.body.message).toBe("SUCCESS");
+
+    })
+})
