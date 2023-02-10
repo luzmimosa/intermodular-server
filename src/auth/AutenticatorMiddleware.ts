e1a174352f5abacdde56cfd341dd01d5e42f92a4
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-10 16:31:57 +0100

    empty authorization tokens won't be invalid anymore, and will be treated as anonymous clients

--- src/auth/AutenticatorMiddleware.ts
+++ src/auth/AutenticatorMiddleware.ts
@@ -5,38 +5,45 @@ const highSecurityMinutes = 20;
 
 export const userValidator = (req: any, res: any, next: any) => {
     const authHeader = req.headers.authorization;
+
     if (authHeader) {
-        const token = authHeader.split(' ')[1];
+        const tokenParts = authHeader.split(' ')[1];
+        if (tokenParts.length >= 2) {
 
-        try {
-            const decodedToken: any = verify(token, process.env.JWT_SECRET !!)
+            const token = tokenParts[1];
 
-            req.permission = RequestPermission.USER;
-            req.username = decodedToken.username
-            req.isLogged = true;
-            req.token = decodedToken
+            try {
 
-            // Check if the token is expired
-            if (decodedToken.exp < Date.now() / 1000) {
-                res.status(401).send("TOKEN_EXPIRED");
+                const decodedToken: any = verify(token, process.env.JWT_SECRET !!)
+
+                req.permission = RequestPermission.USER;
+                req.username = decodedToken.username
+                req.isLogged = true;
+                req.token = decodedToken
+
+                // Check if the token is expired
+                if (decodedToken.exp < Date.now() / 1000) {
+                    res.status(401).send("TOKEN_EXPIRED");
+                    return;
+                }
+
+                // Check if the token is high security (recently created)
+                if (decodedToken.iat + (1000 * 60 * highSecurityMinutes) < Date.now()) {
+                    req.permission = RequestPermission.HIGH_SECURITY_USER;
+                }
+
+                next();
+                return;
+            } catch (e) {
+                console.log("Request with invalid token");
+                console.log(e);
+                res.status(403).json({message: "INVALID_TOKEN"});
                 return;
             }
-
-            // Check if the token is high security (recently created)
-            if (decodedToken.iat + (1000 * 60 * highSecurityMinutes) < Date.now() ) {
-                req.permission = RequestPermission.HIGH_SECURITY_USER;
-            }
-
-            next();
-        } catch (e) {
-            console.log("Request with invalid token");
-            console.log(e);
-            res.status(403).json({message: "INVALID_TOKEN"});
-            return;
         }
-    } else {
-        req.permission = RequestPermission.PUBLIC;
-        req.user = null;
-        next();
     }
+
+    req.permission = RequestPermission.PUBLIC;
+    req.user = null;
+    next();
 }
